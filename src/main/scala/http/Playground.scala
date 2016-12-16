7e2412779b9a44bbe3ae69e6b2ac501b87b0d155
Anthony Garo <Anthony_Garo@s5a.com>
2016-12-16 15:40:11 -0500

    more test routes

---
+++ src/main/scala/http/Playground.scala
@@ -0,0 +1,50 @@
+package http
+
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+
+object Playground extends App {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  * Routes:
+  *
+  * 1. /api/product/{some-segment}
+  * 2. /api/product/{some-segment}/{quantity-value}
+  * 3. /api/product${query-parameters}
+  * 4. /api/products -> redirect ->  /api/product?url=redirectUrl
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  val route1: Route =
+    pathPrefix("api") {
+      pathPrefix("product") {
+        path(Segment) { segment ⇒ // http://localhost:8080/api/product/red
+          complete(s"segment=$segment")
+        } ~
+        parameters('url.as[String]) { url ⇒ // http://localhost:8080/api/product?url=Some
+          get { complete(s"url=$url") }
+        } ~
+        path(Segment / IntNumber.?) { (seg, num) ⇒ // http://localhost:8080/api/product/red/ OR
+          complete(s"seg=$seg :: num=$num") // http://localhost:8080/api/product/red/5
+        }
+      }
+    }
+
+  val route2: Route =
+    pathPrefix("api") {
+      pathPrefix("product") {
+        pathPrefix(Segment) { segment ⇒ // http://localhost:8080/api/product/red
+          path(IntNumber) { num ⇒
+            complete(s"segment=$segment :: num=$num") //http://localhost:8080/api/product/red/5
+          } ~
+          pathEndOrSingleSlash {
+            complete(s"segment=$segment") // http://localhost:8080/api/product/red/ OR http://localhost:8080/api/product/red
+          }
+        } ~
+          parameters('url.as[String]) { url ⇒ // http://localhost:8080/api/product?url=Some
+            get { complete(s"url=$url") }
+          }
+      }
+    }
+
+
+  runSingleRequestServer(route2)
+}
